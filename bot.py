1102a932611f6b97388204bfb681c84cbe6147af
Tuvio717 <46975220+MatteoTuvioGarcia@users.noreply.github.com>
2022-03-02 12:28:33 +0100

    Quick clean up

--- bot.py
+++ bot.py
@@ -1,46 +1,38 @@
 # bot.py
 import os
-import random
-import json
-import discord
 from discord.ext import commands
-from discord.utils import get
 from dotenv import load_dotenv
 
-#____________________CONFIG_AREA_____________________________#
+# ____________________CONFIG_AREA_____________________________#
 
-path="C:\Program Files\HandBrake\HandBrake.exe"
-ExeToKill="HandBrake.exe"
-prefix="!"
-#____________________CONFIG_AREA_____________________________#
+path = "C:/Program Files/HandBrake/HandBrake.exe"
+ExeToKill = "HandBrake.exe"
+prefix = "!"
+# ____________________CONFIG_AREA_____________________________#
 
-
-
+print("hello")
 load_dotenv()
 token = os.getenv('CLIENT_TOKEN')
 bot = commands.Bot(command_prefix=prefix)
 
 
-@bot.event    
+@bot.event
 async def on_message(message):
     if message.author == bot.user:
         return
 
-    if message.content == prefix+"start":
-           member = message.author
-           print("chkpnt")
-           await message.channel.send('Attempting to start server..')
-           os.system('start ""'+' "'+path+'"' )
-           await message.channel.send('Server started successfully. Give it just a few seconds to pop-up ingame.')
-                
-    if message.content == prefix+"stop":
-           member = message.author
-           print("chkpnt2")
-           await message.channel.send('Attempting to stop server..')
-           os.system("TASKKILL /F /IM " +ExeToKill)
-           await message.channel.send('Server stopped successfully.')           
+    if message.content == prefix + "start":
+        print("chkpnt")
+        await message.channel.send('Attempting to start server..')
+        os.system('start ""' + ' "' + path + '"')
+        await message.channel.send('Server started successfully. Give it just a few seconds to pop-up ingame.')
+
+    if message.content == prefix + "stop":
+        print("chkpnt2")
+        await message.channel.send('Attempting to stop server..')
+        os.system("TASKKILL /F /IM " + ExeToKill)
+        await message.channel.send('Server stopped successfully.')
     await bot.process_commands(message)
-    
+
 
 bot.run(token)
-
